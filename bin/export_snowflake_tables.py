8a273ce8916fce44264544c4db91d4c82e439f2b
leonDataNut <83290838+leonDataNut@users.noreply.github.com>
2022-03-30 12:01:02 -0500

    making folder a calling arg

--- bin/export_snowflake_tables.py
+++ bin/export_snowflake_tables.py
@@ -1,4 +1,8 @@
 from migrations.export_snowflake_tables import export_schema_tables_to_csv
+import argparse
 
 if __name__ == '__main__':
-    export_schema_tables_to_csv()
+    parser = argparse.ArgumentParser(description='Process for exporting tables from snowflake to a folder. By default the folder will be Documents')
+    parser.add_argument('folder',type=str, nargs='?', const='Documents', default='Documents')
+    args = parser.parse_args()
+    export_schema_tables_to_csv(folder=args.folder)
